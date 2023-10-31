321c07cf469f11a1d05c1a5280861c9bb2c7a922
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-31 15:15:54 -0500

    Index Routes and contextual errors

--- react-router/react-router-tutorial/src/main.jsx
+++ react-router/react-router-tutorial/src/main.jsx
@@ -5,12 +5,12 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root, {
   loader as rootLoader,
   action as rootAction,
-  action,
 } from "./routes/root";
 import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes/index";
 
 const router = createBrowserRouter([
   {
@@ -20,6 +20,7 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      { index: true, element: <Index /> },
       {
         path: "contacts/:contactId",
         element: <Contact />,
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
       {
         path: "contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>
       },
     ],
   },
--- react-router/react-router-tutorial/src/routes/destroy.jsx
+++ react-router/react-router-tutorial/src/routes/destroy.jsx
@@ -1,7 +1,7 @@
 import { redirect } from "react-router-dom";
 import { deleteContact } from "../contacts";
 
-export default async function action({params}) {
+export async function action({params}) {
   await deleteContact(params.contactId);
   return redirect("/");
 }
