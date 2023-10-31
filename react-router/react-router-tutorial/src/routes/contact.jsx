279fbe4fd17d57c7480725d6275712f82ac7c279
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-31 16:19:34 -0500

    Not Found Data in loader contact

--- react-router/react-router-tutorial/src/routes/contact.jsx
+++ react-router/react-router-tutorial/src/routes/contact.jsx
@@ -82,5 +82,13 @@ export async function action({ request, params }) {
 
 export async function loader({ params }) {
   const contact = await getContact(params.contactId);
+
+  if (!contact) {
+    throw new Response("", {
+      status: 404,
+      statusText: "Not Found",
+    });
+  }
+
   return { contact };
 }
