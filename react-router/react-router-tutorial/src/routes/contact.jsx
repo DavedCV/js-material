a014250f6163fbdb172dc8d56823f155293f9d91
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-10-31 13:59:10 -0500

    URL Params in Loaders

--- react-router/react-router-tutorial/src/routes/contact.jsx
+++ react-router/react-router-tutorial/src/routes/contact.jsx
@@ -1,14 +1,8 @@
-import { Form } from "react-router-dom";
+import { Form, useLoaderData } from "react-router-dom";
+import { getContact } from "../contacts";
 
 export default function Contact() {
-  const contact = {
-    first: "Your",
-    last: "Name",
-    avatar: "https://placekitten.com/g/200/200",
-    twitter: "your_handle",
-    notes: "Some notes",
-    favorite: true,
-  };
+  const { contact } = useLoaderData();
 
   return (
     <div id="contact">
@@ -72,4 +66,9 @@ function Favorite({ contact }) {
       </button>
     </Form>
   );
+}
+
+export async function loader({ params }) {
+  const contact = await getContact(params.contactId);
+  return { contact };
 }
