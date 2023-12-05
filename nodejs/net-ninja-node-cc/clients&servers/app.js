7f75f6a247b14b2e7908983ef115d138e420e1f2
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-12-05 08:40:34 -0500

    nodejs: add mongo and mongoose to persist data

--- nodejs/net-ninja-node-cc/clients&servers/app.js
+++ nodejs/net-ninja-node-cc/clients&servers/app.js
@@ -1,37 +1,43 @@
 const express = require("express");
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
 
 const app = express();
 
-app.listen(3000);
+// to connect to mongodb
+const dbURI =
+  "mongodb+srv://davidnode:test1234@nodetuts.irfnmwr.mongodb.net/nodetuts?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI)
+  .then((result) => app.listen(3000))
+  .catch((err) => console.log(err));
+
+// template engine config
 app.set("view engine", "ejs");
 app.set("views", "clients&servers/views/");
 
+// add static files
 app.use(express.static("clients&servers/public"));
 
+// add middleware for logging
 app.use(morgan("dev"));
 
 app.get("/", (req, res) => {
-  const blogs = [
-    {
-      title: "Yoshi finds eggs",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "Mario finds stars",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-    {
-      title: "How to defeat bowser",
-      snippet: "Lorem ipsum dolor sit amet consectetur",
-    },
-  ];
-
-  res.render("index", { title: "Home", blogs });
+  res.redirect("/blogs");
 });
 
 app.get("/about", (req, res) => {
   res.render("about", { title: "About" });
+});
+
+app.get("/blogs", (req, res) => {
+  Blog.find()
+    .then((results) => {
+      res.render("index", { title: "All blogs", blogs: results });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get("/blogs/create", (req, res) => {
