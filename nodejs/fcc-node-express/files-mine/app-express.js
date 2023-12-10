0216c7116a020dc287dadd45d5d86f14aa8c3710
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-12-10 10:59:17 -0500

    nodejs: ultra basic methods to test post requests

--- nodejs/fcc-node-express/files-mine/app-express.js
+++ nodejs/fcc-node-express/files-mine/app-express.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const path = require("path");
-const { products } = require("./data");
+const { products, people } = require("./data");
 
 const app = express();
 
-// allow responses to static files
 // setup static and middleware
-app.use(express.static("./public"));
+//app.use(express.static("./public"));
+// setup static route middleware to test post requests
+app.use(express.static("./methods-public"));
+// parse urlencoded form data
+app.use(express.urlencoded({ extended: false }));
+// parse json data
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>Home page</h1><a href='/api/products'>products</a>");
@@ -50,6 +55,31 @@ app.get("/api/query", (req, res) => {
   }
 
   res.status(200).json(sortedProducts);
+});
+
+app.get("/api/people", (req, res) => {
+  res.status(200).json({ sucess: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+
+  if (!name)
+    return res
+      .status(400)
+      .json({ sucess: false, msg: "please provide name value" });
+
+  res.status(201).json({ sucess: true, person: name });
+});
+
+app.post("/login", (req, res) => {
+  console.log(req.body);
+
+  const { name } = req.body;
+
+  if (!name) return res.status(401).send("Please provide credentials!");
+
+  res.status(200).send(`Welcome ${name}!`);
 });
 
 app.all("*", (req, res) => {
