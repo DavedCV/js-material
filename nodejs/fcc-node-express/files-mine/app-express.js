9c99f330a8b1ee7305c6e3b683953f9be3ccb754
DavedCV <97773641+DavedCV@users.noreply.github.com>
2023-12-10 11:39:40 -0500

    nodejs: add sample method to show ultra simple functionality to a put req

--- nodejs/fcc-node-express/files-mine/app-express.js
+++ nodejs/fcc-node-express/files-mine/app-express.js
@@ -82,6 +82,39 @@ app.post("/login", (req, res) => {
   res.status(200).send(`Welcome ${name}!`);
 });
 
+app.put("/api/people/:id", (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  if (!id || !name)
+    return res
+      .status(400)
+      .json({ sucess: false, msg: "should provide an a id and a name" });
+
+  people.map((person) => {
+    if (person.id === +id) {
+      person.name = name;
+    }
+
+    return person;
+  });
+
+  res.status(200).json({ sucess: true, data: people });
+});
+
+app.delete("/api/people/:id", (req, res) => {
+  const person = people.find((person) => person.id === Number(req.params.id));
+
+  if (!person) {
+    return res
+      .status(404)
+      .json({ success: false, msh: `No person with id: ${req.params.id}` });
+  }
+
+  const newPeople = people.filter((person) => person.id !== +req.params.id);
+  res.status(200).json({ success: true, data: newPeople });
+});
+
 app.all("*", (req, res) => {
   res.status(404).send("resource nor found!");
 });
